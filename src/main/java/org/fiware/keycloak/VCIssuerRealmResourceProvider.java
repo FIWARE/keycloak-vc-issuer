d947ee99b4a9ce676e935109d5aa0c53fb0c4176
Stefan Wiedemann <wistefan@googlemail.com>
2023-06-29 15:59:28 +0200

    hard

--- src/main/java/org/fiware/keycloak/VCIssuerRealmResourceProvider.java
+++ src/main/java/org/fiware/keycloak/VCIssuerRealmResourceProvider.java
@@ -215,30 +215,30 @@ public class VCIssuerRealmResourceProvider implements RealmResourceProvider {
 		}
 	}
 
-	/**
-	 * Returns the meta data of the issuer.
-	 */
+//	/**
+//	 * Returns the meta data of the issuer.
+//	 */
+//	@GET
+//	@Path("{issuer-did}/.well-known/openid-credential-issuer")
+//	@Produces({ MediaType.APPLICATION_JSON })
+//	@ApiOperation(value = "Return the issuer metadata", notes = "https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#name-credential-issuer-metadata-", tags = {})
+//	@ApiResponses(value = {
+//			@ApiResponse(code = 200, message = "The credentials issuer metadata", response = CredentialIssuerVO.class) })
+//	public Response getIssuerMetadata(@PathParam("issuer-did") String issuerDidParam) {
+//		LOGGER.info("Retrieve issuer meta data");
+//		assertIssuerDid(issuerDidParam);
+//
+//		KeycloakContext currentContext = session.getContext();
+//
+//		return Response.ok().entity(new CredentialIssuerVO()
+//						.credentialIssuer(getIssuer())
+//						.credentialEndpoint(getCredentialEndpoint())
+//						.credentialsSupported(getSupportedCredentials(currentContext)))
+//				.header(ACCESS_CONTROL_HEADER, "*").build();
+//	}
+
 	@GET
 	@Path("{issuer-did}/.well-known/openid-credential-issuer")
-	@Produces({ MediaType.APPLICATION_JSON })
-	@ApiOperation(value = "Return the issuer metadata", notes = "https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#name-credential-issuer-metadata-", tags = {})
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "The credentials issuer metadata", response = CredentialIssuerVO.class) })
-	public Response getIssuerMetadata(@PathParam("issuer-did") String issuerDidParam) {
-		LOGGER.info("Retrieve issuer meta data");
-		assertIssuerDid(issuerDidParam);
-
-		KeycloakContext currentContext = session.getContext();
-
-		return Response.ok().entity(new CredentialIssuerVO()
-						.credentialIssuer(getIssuer())
-						.credentialEndpoint(getCredentialEndpoint())
-						.credentialsSupported(getSupportedCredentials(currentContext)))
-				.header(ACCESS_CONTROL_HEADER, "*").build();
-	}
-
-	@GET
-	@Path("{issuer-did}/alt/.well-known/openid-credential-issuer")
 	@Produces({ MediaType.APPLICATION_JSON })
 	@ApiOperation(value = "Return the issuer metadata", notes = "https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#name-credential-issuer-metadata-", tags = {})
 	@ApiResponses(value = {
